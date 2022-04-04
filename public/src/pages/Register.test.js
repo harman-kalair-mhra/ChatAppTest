08452e003243d54838988a516db2a2a9280162be
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2022-04-05 00:05:36 +0100

    first commit

---
+++ public/src/pages/Register.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import toJSON from "enzyme-to-json"
+import { shallow } from "enzyme"
+
+import Register from './Register';
+import Button from './Register'
+import Input from './Register'
+
+
+describe("Smoke test", () => {
+    it("1 + 1 equals 2", () => {
+        expect(1+1).toEqual(2)
+    })
+})
+
+describe("Rendering Test", () => {
+    test("Test to check, if component renders correctly", () => {
+    const wrapper = shallow(<Register/>)
+
+    expect(toJSON(wrapper)).toMatchSnapshot()
+})
+})
+
+describe('Button', () => {
+    
+    test('should be defined', () => {
+      expect(Button).toBeDefined();
+    });
+    
+    test('test to check, if button renders correctly', () => {
+      const wrapper = shallow(
+        <Button name='button test' />
+      );
+      expect(wrapper).toMatchSnapshot();
+    });
+   });
+
+   describe('Input', () => {
+    test('should be defined', () => {
+        expect(Input).toBeDefined()
+    })
+
+    test('test to check, if input field renders correctly', () => {
+     const wrapper = shallow(
+       <Input name='input test' />
+     );
+     expect(wrapper).toMatchSnapshot();
+   });
+})
+   
+
+
